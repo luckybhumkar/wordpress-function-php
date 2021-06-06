95853736482db75ff6be7a2ee5742b50c2bb102f
luckybhumkar <ls.bhumkar@gmail.com>
2021-06-06 19:58:19 +0530

    Create lazy-load-adsense.php

---
+++ lazy-load-adsense.php
@@ -0,0 +1,23 @@
+
+add_action('wp_footer', 'your_function_name');
+function your_function_name(){
+?>
+
+<!-- Lazy load AdSense-->
+<!--noptimize-->
+<script type="text/javascript">
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script>
+<!--/noptimize-->
+<?php
+}};
+//END: Custom JS script at footer
